656dd2983b7f42f1da3fc4273cb304b6ae27ff6e
Waithaka <kennwaithaka@gmail.com>
2025-09-28 00:53:35 +0300

    Add test case for the hook list copy method

--- hooks_test.go
+++ hooks_test.go
@@ -8,6 +8,45 @@ import (
 	"github.com/SirWaithaka/gorequest"
 )
 
+func TestHookList_Copy(t *testing.T) {
+
+	// create a hook list with 4 hooks
+	list := gorequest.HookList{}
+	list.PushBackHook(gorequest.Hook{Name: "Foo", Fn: func(r *gorequest.Request) {}})
+	list.PushBackHook(gorequest.Hook{Name: "Bar", Fn: func(r *gorequest.Request) {}})
+	list.PushBackHook(gorequest.Hook{Name: "Baz", Fn: func(r *gorequest.Request) {}})
+	list.PushBackHook(gorequest.Hook{Name: "Qux", Fn: func(r *gorequest.Request) {}})
+	// set hooks
+	hooks := gorequest.Hooks{}
+	hooks.Validate = list
+	hooks.Build = list
+	hooks.Send = list
+	hooks.Unmarshal = list
+	hooks.Complete = list
+	// create a copy
+	copied := hooks.Copy()
+	// assert that the number of items in copy and original are the same
+	assert.Equal(t, hooks.Validate.Len(), copied.Validate.Len())
+	assert.Equal(t, hooks.Build.Len(), copied.Build.Len())
+	assert.Equal(t, hooks.Send.Len(), copied.Send.Len())
+	assert.Equal(t, hooks.Unmarshal.Len(), copied.Unmarshal.Len())
+	assert.Equal(t, hooks.Complete.Len(), copied.Complete.Len())
+
+	// delete an item from the original
+	hooks.Validate.Remove("Bar")
+	hooks.Build.Remove("Baz")
+	hooks.Send.Remove("Qux")
+	hooks.Unmarshal.Remove("Foo")
+	hooks.Complete.Remove("Qux")
+	// assert that the copy has the removed item
+	assert.Equal(t, 4, copied.Validate.Len())
+	assert.Equal(t, 4, copied.Build.Len())
+	assert.Equal(t, 4, copied.Send.Len())
+	assert.Equal(t, 4, copied.Unmarshal.Len())
+	assert.Equal(t, 4, copied.Complete.Len())
+
+}
+
 func TestHookList_Run(t *testing.T) {
 	r := &gorequest.Request{}
 	h := gorequest.HookList{}
